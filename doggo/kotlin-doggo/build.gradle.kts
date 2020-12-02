4f35c2b0fe0629aa36e4fa9bce78b8f2f5fa5254
Victor Yuri Alves Tripeno <victoryuri91@hotmail.com>
2020-12-01 21:09:11 -0300

    add pact plugin

--- doggo/kotlin-doggo/build.gradle.kts
+++ doggo/kotlin-doggo/build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
 	kotlin("jvm") version "1.4.10"
 	kotlin("plugin.spring") version "1.4.10"
+	id("au.com.dius.pact") version "4.1.2"
+	id("org.unbroken-dome.test-sets") version "2.2.1"
 }
 
 group = "com.doggo"
@@ -15,12 +17,27 @@ repositories {
 	mavenCentral()
 }
 
+testSets {
+	"componentTest"()
+	"contractTest"()
+}
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	"componentTestImplementation"(sourceSets["test"].output)
+	"contractTestImplementation"(sourceSets["test"].output)
+	"contractTestImplementation"("au.com.dius.pact.consumer:junit5:4.1.0")
+}
+
+pact {
+	publish {
+		pactBrokerUrl = "https://pact-broker.dev.doggo"
+	}
 }
 
 tasks.withType<KotlinCompile> {
